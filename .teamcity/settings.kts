bb5746c9ed0d7a20e2c6de0f827e4a90adb3bbe2
Tom Bamford <tom@bamford.io>
2020-08-19 11:57:02 +0100

    wip teamcity

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -8,9 +8,7 @@ var clientId = DslContext.getParameter("clientId", "")
 var clientSecret = DslContext.getParameter("clientSecret", "")
 var tenantId = DslContext.getParameter("tenantId", "")
 var environment = DslContext.getParameter("environment", "public")
-var clientIdAlt = DslContext.getParameter("clientIdAlt", "")
-var clientSecretAlt = DslContext.getParameter("clientSecretAlt", "")
 
-var clientConfig = ClientConfiguration(clientId, clientSecret, tenantId, clientIdAlt, clientSecretAlt)
+var clientConfig = ClientConfiguration(clientId, clientSecret, tenantId)
 
 project(AzureAD(environment, clientConfig))
